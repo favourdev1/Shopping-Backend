bc98e6dcbd8b575d38833d438568ba3df4ddd433
favourdev1 <favourapps17@gmail.com>
2024-03-06 03:08:23 +0100

    Refactor OrderController and add fetchOrderbyOrderNumber method

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -3,21 +3,24 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\AdminSettings;
+use App\Models\Order;
 use Illuminate\Http\Request;
 use illuminate\Http\facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
+
 class OrderController extends Controller
 {
-    
-    public  function generateOrderId()
+    public function generateOrderId()
     {
         // function to generate unique combination of letters and numbers
-        $orderId = 'ORD' . time() . self::generateUniqueCode(
-            5
-        );
+        do {
+            $orderId = 'ORD' . time() . self::generateUniqueCode(5);
+            $orders = Order::where('order_number',$orderId)->first();
+        } while($orders);
+    
         return $orderId;
-        
     }
 
     public function generateUniqueCode($length = 8)
@@ -31,63 +34,69 @@ class OrderController extends Controller
         return $code;
     }
 
-    // fetch all the orders for a particular user 
+    // fetch all the orders for a particular user
     public function fetchOrders()
     {
         $user = auth()->user();
 
         $orders = [];
         if ($user) {
-            $orders = $user->orders()->join('order_items', 'orders.id', '=', 'order_items.order_id')
-            ->join('products','products.id','=','order_items.product_id')->get();
+            $orders = $user->orders()->join('order_items', 'orders.id', '=', 'order_items.order_id')->join('products', 'products.id', '=', 'order_items.product_id')->get();
         } else {
             return response()->json(['message' => 'User not authenticated'], 401);
         }
-        return response()->json([
-            'data' => $orders,
-            'message' => 'Orders fetched successfully.',
-            'status' => 'success'
-        ], 200);
+        return response()->json(
+            [
+                'data' => $orders,
+                'message' => 'Orders fetched successfully.',
+                'status' => 'success',
+            ],
+            200,
+        );
     }
 
-
-    public function fetchOrderbyOrderNumber (){
+    public function fetchOrderbyOrderNumber($order_number)
+    {
         $user = auth()->user();
-        if(!$user){
+        if (!$user) {
             return response()->json(['message' => 'User not authenticated'], 401);
         }
-// run validation 
-        $validator = Validator::make(request()->all(), [
-            'order_number' => 'required|exists:orders,order_number',
-        ]);
+        // run validation
+        $validator = Validator::make(
+            ['order_number' => $order_number],
+            [
+                'order_number' => 'required',
+            ],
+        );
 
         if ($validator->fails()) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Validation failed',
-                'errors' => $validator->errors(),
-            ], 422);
+            return response()->json(
+                [
+                    'status' => 'error',
+                    'message' => 'Validation failed',
+                    'errors' => $validator->errors(),
+                ],
+                422,
+            );
         }
 
 
-
-
-        $orderNumber = request()->input('order_number');
-
-        $order = $user->orders()->where('order_number', $orderNumber)->first();
+        $order = $user->orders()->where('order_number', $order_number)->first();
+        $adminSettings = AdminSettings::first();
         if (!$order) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Order not found',
-            ], 404);
+            return response()->json(
+                [
+                    'status' => 'error',
+                    'message' => 'Order not found order number = '.$order_number,
+                ],
+                200,
+            );
         }
 
         return response()->json([
             'status' => 'success',
             'message' => 'Order fetched successfully',
-            'data' => $order,
+            'data' => ['admin_settings'=>$adminSettings,'order'=>$order]
         ]);
-
     }
-
 }
